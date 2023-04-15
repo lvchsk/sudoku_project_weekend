c61bc8236356b885d60ccf13590a0b95133cfdeb
lvchsk <dienislev@mail.ru>
2023-04-15 20:51:01 +0300

    initial commit

---
+++ checkCurrent.js
@@ -0,0 +1,17 @@
+const check = (number, position, array) => {
+  const [x, y] = position;
+
+  for (let i = 0; i < 9; i += 1) {
+    if (array[i][y] === number && i !== x) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < 9; i += 1) {
+    if (array[x][i] === number && i !== y) {
+      return false;
+    }
+  }
+
+  return true;
+};
